1c360b210180f3752b2abd147f5fd754b440f354
konga22 <konga22@users.noreply.github.com>
2026-04-30 16:44:16 +0900

    회원가입 API 연동 및 화면 수정

--- lib/app/extensions/snackbar_extension.dart
+++ lib/app/extensions/snackbar_extension.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 extension SnackbarExtension on BuildContext {
-  void showSnackbar(String message) {
+  void showSnackbar(String message, {bool isError = false}) {
     ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(content: Text(message)),
+      SnackBar(
+        backgroundColor: isError ? Theme.of(this).colorScheme.error : null,
+        content: Text(message),
+      ),
     );
   }
 }
--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:unitask/app/extensions/snackbar_extension.dart';
+import 'package:unitask/services/api_service.dart';
 import 'package:unitask/ui/common/label_text_field.dart';
 
 class SignupPage extends StatefulWidget {
@@ -15,28 +17,55 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController _passwordController = .new();
   final TextEditingController _passwordConfirmController = .new();
 
+  bool _loading = false;
+
+  void _startLoading() => setState(() => _loading = true);
+  void _stopLoading() => setState(() => _loading = false);
+
+  void onSignup() async {
+    final name = _nameController.text.trim();
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+    final passwordConfirm = _passwordConfirmController.text.trim();
+
+    if (name.isEmpty ||
+        email.isEmpty ||
+        password.isEmpty ||
+        passwordConfirm.isEmpty) {
+      context.showSnackbar('정보가 올바르지 않습니다.', isError: true);
+      return;
+    }
+
+    if (password != passwordConfirm) {
+      context.showSnackbar('비밀번호가 일치하지 않습니다.', isError: true);
+      return;
+    }
+
+    _startLoading();
+
+    final signupResult = await ApiService.signup(
+      email: email,
+      password: password,
+      name: name,
+    );
+
+    _stopLoading();
+
+    if (signupResult == null) return;
+
+    if (!signupResult) {
+      if (mounted) context.showSnackbar('계정 생성에 실패했습니다.', isError: true);
+    }
+    return;
+  }
+
   @override
   void dispose() {
     _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     _passwordConfirmController.dispose();
-
     super.dispose();
-  }
-
-  void _onSignup() {
-    debugPrint('계정 만들기');
-
-    final name = _nameController.text;
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    final passwordConfirm = _passwordConfirmController.text;
-
-    debugPrint('이름: $name');
-    debugPrint('이메일: $email');
-    debugPrint('비밀번호: $password');
-    debugPrint('비밀번호 확인: $passwordConfirm');
   }
 
   @override
@@ -44,44 +73,65 @@ class _SignupPageState extends State<SignupPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('회원가입', style: TextStyle(fontWeight: .bold)),
+        title: const Text(
+          '회원가입',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Padding(
-        padding: .all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
-          spacing: 25,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            LabelTextField(
-              controller: _nameController,
-              label: '이름',
-              icon: LucideIcons.userRoundPen,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: LabelTextField(
+                controller: _nameController,
+                label: '이름',
+                icon: LucideIcons.userRoundPen,
+              ),
             ),
-            LabelTextField(
-              controller: _emailController,
-              label: '이메일',
-              icon: LucideIcons.mail,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: LabelTextField(
+                controller: _emailController,
+                label: '이메일',
+                icon: LucideIcons.mail,
+              ),
             ),
-            LabelTextField(
-              controller: _passwordController,
-              label: '비밀번호',
-              enableObscure: true,
-              icon: LucideIcons.lockKeyhole,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: LabelTextField(
+                controller: _passwordController,
+                label: '비밀번호',
+                icon: LucideIcons.lockKeyholeOpen,
+                enableObscure: true,
+              ),
             ),
-            LabelTextField(
-              controller: _passwordConfirmController,
-              label: '비밀번호 확인',
-              enableObscure: true,
-              icon: LucideIcons.lockKeyholeOpen,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: LabelTextField(
+                controller: _passwordConfirmController,
+                label: '비밀번호 확인',
+                icon: LucideIcons.lockKeyholeOpen,
+                enableObscure: true,
+              ),
             ),
-            SizedBox(
-              width: .infinity,
-              child: ElevatedButton(
-                onPressed: _onSignup,
-                child: Text(
-                  '계정 만들기',
-                  style: TextStyle(fontWeight: .bold, fontSize: 20),
+            ElevatedButton(
+              onPressed: onSignup,
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(55),
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
+              child: _loading
+                  ? const SizedBox.square(
+                      dimension: 30,
+                      child: CircularProgressIndicator(color: Colors.white),
+                    )
+                  : const Text('계정 만들기'),
             ),
           ],
         ),
